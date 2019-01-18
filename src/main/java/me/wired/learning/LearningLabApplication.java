da7bbac8e86f0248788cba23423d4e6836a26a67
DandyU <niceyuseon@gmail.com>
2019-01-18 10:48:10 +0900

    Add project files

---
+++ src/main/java/me/wired/learning/LearningLabApplication.java
@@ -0,0 +1,14 @@
+package me.wired.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningLabApplication.class, args);
+    }
+
+}
+
